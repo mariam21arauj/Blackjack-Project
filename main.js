91bb8be097cac6d4622c7c77ab7f83ea62e7e2ed
Mariam Araujo <mariam21betancourt@gmail.com>
2023-04-03 22:04:41 -0700

    WIP: Added html, started CSS

---
+++ main.js
@@ -0,0 +1,15 @@
+alert("Hello! I am an alert box!!");
+
+  /*----- constants -----*/
+
+
+  /*----- state variables -----*/
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+
+
+  /*----- functions -----*/
