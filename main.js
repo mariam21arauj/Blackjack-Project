0a00f8d11bbb94877849cacbc5f30ade3ac5e3c2
Mariam Araujo <mariam21betancourt@gmail.com>
2023-04-04 16:43:43 -0700

    WIP, added card values and images the long way. saving just in case but will change it

--- main.js
+++ main.js
@@ -1,9 +1,68 @@
-alert("Hello! I am an alert box!!");
-
   /*----- constants -----*/
+ const cards = [
+    {suit: 'Spades', value: 'Ace', numValue: 11, img: "../images/spades/spades-A.svg"},
+    {suit: 'Spades', value: 'K', numValue:10, img:"../images/spades/spades-K.svg" },
+    {suit: 'Spades', value: 'Q', numValue:10, img:"../images/spades/spades-Q.svg" },
+    {suit: 'Spades', value: 'J', numValue:10, img:"../images/spades/spades-J.svg" },
+    {suit: 'Spades', value: '10', numValue:10, img:"../images/spades/spades-r10.svg" },
+    {suit: 'Spades', value: '9', numValue:9, img:"../images/spades/spades-r09.svg" },
+    {suit: 'Spades', value: '8', numValue:8, img:"../images/spades/spades-r08.svg" },
+    {suit: 'Spades', value: '7', numValue:7, img:"../images/spades/spades-r07.svg" },
+    {suit: 'Spades', value: '6', numValue:6, img:"../images/spades/spades-r06.svg" },
+    {suit: 'Spades', value: '5', numValue:5, img:"../images/spades/spades-r05.svg" },
+    {suit: 'Spades', value: '4', numValue:4, img:"../images/spades/spades-r04.svg" },
+    {suit: 'Spades', value: '3', numValue:3, img:"../images/spades/spades-r03.svg" },
+    {suit: 'Spades', value: '2', numValue:2, img:"../images/spades/spades-r02.sv" },
+    {suit: 'Diamonds', value: 'Ace', numValue:11, img:"" },
+    {suit: 'Diamonds', value: 'K', numValue:10, img:"" },
+    {suit: 'Diamonds', value: 'Q', numValue:10, img:"" },
+    {suit: 'Diamonds', value: 'J', numValue:10, img:"" },
+    {suit: 'Diamonds', value: '10', numValue:10, img:"" },
+    {suit: 'Diamonds', value: '9', numValue:9, img:"" },
+    {suit: 'Diamonds', value: '8', numValue:8, img:"" },
+    {suit: 'Diamonds', value: '7', numValue:7, img:"" },
+    {suit: 'Diamonds', value: '6', numValue:6, img:"" },
+    {suit: 'Diamonds', value: '5', numValue:5, img:"" },
+    {suit: 'Diamonds', value: '4', numValue:4, img:"" },
+    {suit: 'Diamonds', value: '3', numValue:3, img:"" },
+    {suit: 'Diamonds', value: '2', numValue:2, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+    {suit: '', value: '', numValue:, img:"" },
+ ]
+
 
 
   /*----- state variables -----*/
+  let playerScore;
+  let dealerScore; 
+  let dealtMoney;
+ 
+
 
 
   /*----- cached elements  -----*/
