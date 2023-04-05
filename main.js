efd2c02141170e4392c6050b1cd0794081ceabf0
Mariam Araujo <mariam21betancourt@gmail.com>
2023-04-04 22:20:01 -0700

    change card/value/image with a shuffle function

--- main.js
+++ main.js
@@ -1,58 +1,7 @@
   /*----- constants -----*/
-  const cards = [
-    {numValue: 11, img: "../images/spades/spades-A.svg"},
-    {numValue:10, img:"../images/spades/spades-K.svg" },
-    {numValue:10, img:"../images/spades/spades-Q.svg" },
-    {numValue:10, img:"../images/spades/spades-J.svg" },
-    {numValue:10, img:"../images/spades/spades-r10.svg" },
-    {numValue:9, img:"../images/spades/spades-r09.svg" },
-    {numValue:8, img:"../images/spades/spades-r08.svg" },
-    {numValue:7, img:"../images/spades/spades-r07.svg" },
-    {numValue:6, img:"../images/spades/spades-r06.svg" },
-    {numValue:5, img:"../images/spades/spades-r05.svg" },
-    {numValue:4, img:"../images/spades/spades-r04.svg" },
-    {numValue:3, img:"../images/spades/spades-r03.svg" },
-    {numValue:2, img:"../images/spades/spades-r02.sv" },
-    {numValue:11, img:"../images/diamonds/diamonds-A.svg" },
-    {numValue:10, img:"../images/diamonds/diamonds-K.svg" },
-    {numValue:10, img:"../images/diamonds/diamonds-Q.svg" },
-    {numValue:10, img:"../images/diamonds/diamonds-J.svg" },
-    {numValue:10, img:"../images/diamonds/diamonds-r10.svg" },
-    {numValue:9, img:"../images/diamonds/diamonds-r09.svg" },
-    {numValue:8, img:"../images/diamonds/diamonds-r08.svg" },
-    {numValue:7, img:"../images/diamonds/diamonds-r07.svg" },
-    {numValue:6, img:"../images/diamonds/diamonds-r06.svg" },
-    {numValue:5, img:"../images/diamonds/diamonds-r05.svg" },
-    {numValue:4, img:"../images/diamonds/diamonds-r04.svg" },
-    {numValue:3, img:"../images/diamonds/diamonds-r03.svg" },
-    {numValue:2, img:"../images/diamonds/diamonds-r02.svg" },
-    {numValue:11, img:"../images/hearts/hearts-A.svg" },
-    {numValue:10, img:"../images/hearts/hearts-K.svg" },
-    {numValue:10, img:"../images/hearts/hearts-Q.svg" },
-    {numValue:10, img:"../images/hearts/hearts-J.svg" },
-    {numValue:10, img:"../images/hearts/hearts-r10.svg" },
-    {numValue:9, img:"../images/hearts/hearts-r09.svg" },
-    {numValue:8, img:"../images/hearts/hearts-r08.svg" },
-    {numValue:7, img:"../images/hearts/hearts-r07.svg" },
-    {numValue:6, img:"../images/hearts/hearts-r06.svg" },
-    {numValue:5, img:"../images/hearts/hearts-r05.svg" },
-    {numValue:4, img:"../images/hearts/hearts-r04.svg" },
-    {numValue:3, img:"../images/hearts/hearts-r03.svg" },
-    {numValue:2, img:"../images/hearts/hearts-r02.svg" },
-    {numValue:11, img:"../images/clubs/clubs-A.svg" },
-    {numValue:10, img:"../images/clubs/clubs-K.svg" },
-    {numValue:10, img:"../images/clubs/clubs-Q.svg" },
-    {numValue:10, img:"../images/clubs/clubs-J.svg" },
-    {numValue:10, img:"../images/clubs/clubs-r10.svg" },
-    {numValue:9, img:"../images/clubs/clubs-r09.svg" },
-    {numValue:8, img:"../images/clubs/clubs-r08.svg" },
-    {numValue:7, img:"../images/clubs/clubs-r07.svg" },
-    {numValue:6, img:"../images/clubs/clubs-r06.svg" },
-    {numValue:5, img:"../images/clubs/clubs-r05.svg" },
-    {numValue:4, img:"../images/clubs/clubs-r04.svg" },
-    {numValue:3, img:"../images/clubs/clubs-r03.svg" },
-    {numValue:2, img:"../images/clubs/clubs-r02.svg" },
- ]
+  const suits = ['heart', 'spade', 'club', 'diamonds']
+  const values = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+   
 
 
 
@@ -62,29 +11,65 @@
   let dealtMoney;
   let winner;
   let board;
+  let card;
+  let cards; 
  
 
 
 
   /*----- cached elements  -----*/
- const playNowBtn = document.getElementById('playNowButton');
- const hitBtn = document.getElementById('hitButton')
- const stayBtn = document.getElementById('stayButton')
+//  const playNowBtn = document.getElementById('playNowButton');
+//  const hitBtn = document.getElementById('hitButton')
+//  const stayBtn = document.getElementById('stayButton')
 
-  /*----- event listeners -----*/
-  playNowBtn.addEventListener('click', initialize);
-  hitBtn.addEventListener('click', shuffleDeck);
-  stayBtn.addEventListener('click', dealersTurn);
+//   /*----- event listeners -----*/
+//   playNowBtn.addEventListener('click', initialize);
+//   hitBtn.addEventListener('click', shuffleDeck);
+//   stayBtn.addEventListener('click', dealersTurn);
 
   /*----- functions -----*/
 initialize(); 
+
+
+cards = []
+card = {}
+function createCardDeck(suits, values) {
+    for(let i = 0; i < suits.length; i++){
+        for(let j = 0; j < values.length; j++){
+            if(values[j] === 'A'){
+                card = {
+                    cardName: suits[i] + '-' + values[j], 
+                    value: 11,
+                    img: '/images/' + suits[i] + '-' + values[j] + '.svg'
+                }
+            }else if(values[j] === 'K' || values[j] === 'Q' || values[j] === 'J'){
+                card = {
+                    cardName: suits[i] + '-' + values[j], 
+                    value: 10,
+                    img: '/images/' + suits[i] + '-' + values[j] + '.svg'
+                }
+            }else{
+                card = {
+                    cardName: suits[i] + '-' + values[j], 
+                    value: Number(values[j]),
+                    img: '/images/' + suits[i] + '-' + values[j] + '.svg'
+                }
+            }
+         cards.push(card)
+        }
+    }
+    return(cards)
+
+}
+console.log(createCardDeck(suits, values))
+
 
 function initialize(){
     // restarts board
     winner = null;
     board = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0], //Dealers side of the board 
+        [0, 0, 0, 0, 0, 0], //Playeers side of the board
     ];
-    render();
+    // render();
 }
