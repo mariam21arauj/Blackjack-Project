6663d695716aaf46e1ce101b9ed6db5b74ba8a07
Mariam Araujo <mariam21betancourt@gmail.com>
2023-04-06 01:14:22 -0700

    adds logic for hit button and sum of values of cards

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
   /*------------------------ constants -------------------------*/
   const suits = ['hearts', 'spades', 'clubs', 'diamonds']
   const values = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
-   
   /*--------------------- state variables ----------------------*/
   let playerScore;
   let dealerScore; 
@@ -11,7 +10,7 @@
   let winner;
   let board;
   let card;
-  let cards; 
+  let mazo;
   let randCard;
 
 /*---------------------- cached elements ------------------------*/
@@ -24,18 +23,20 @@ const stayBtn = document.getElementById('stayButton')
 
 /*--------------------- event listeners --------------------------*/
 playNowBtn.addEventListener('click', initialize);
-// hitBtn.addEventListener('click', renderBoard);
-// stayBtn.addEventListener('click', dealersTurn);
 
+
+hitBtn.addEventListener('click', renderPlayerChoice);
+// stayBtn.addEventListener('click', dealersTurn);
 
 /*---------------------- functions -----------------------------*/
 
-cards = []
+
 card = {}
 // Every time his function is called, It will generate a card deck matching the values of 
 // my suits and values constants, as well as their respective numerical value and image using 
 // concatenating. 
 function createCardDeck(cardSuits, cardValues) {
+   let cards = []
     for(let i = 0; i < suits.length; i++){
         for(let j = 0; j < values.length; j++){
             if(values[j] === 'A'){
@@ -66,9 +67,11 @@ function createCardDeck(cardSuits, cardValues) {
          cards.push(card)
         }
     }
+
     return(cards)
 
 }
+ mazo = createCardDeck(suits, values)
 
 let randCardValue;
 let randCardImg;
@@ -76,7 +79,7 @@ function shuffleDeck(cardDeck){
         randCard = Math.floor((Math.random() * cardDeck.length));
         return cardDeck[randCard]
      } 
-     console.log(shuffleDeck(createCardDeck(suits, values)))
+     console.log(shuffleDeck(mazo))
 
 function obtainCardValue(cardDeck){
          randCardValue = randCard
@@ -88,11 +91,11 @@ function obtainCardImg(cardDeck){
     randCardImg = randCard
    return cardDeck[randCard].img
     }
-    console.log(obtainCardImg(createCardDeck(suits, values)))
+    console.log(obtainCardImg(mazo))
 
 
 function initialize(){
-    winner = null;  
+    winner = null; 
  render();
     }
 
@@ -103,21 +106,55 @@ function render() {
 // winner message 
    renderWinnerMessage();
 // Allows palyer to click hit and render a card on board
-    renderPlayer();
+    renderPlayerChoice();
  }
 
+
+
+ //These arrays are meant for support to indicate the occupied slots with a 1, and the empty one with a 0
+//               0  1  2  3  4  5
+ boardPlayer =  [0, 0, 0, 0, 0, 0]; //
+ boardDealer =  [0, 0, 0, 0, 0, 0];
+
+    
+
 function renderBoard(){
+    let sum1=0
+    let sum2=0
     for(let i = 0; i < 2; i++ ){
+        // this renders the board for the player
         const addCardImagePlayer = document.createElement('img')
-        shuffleDeck(createCardDeck(suits, values))
-        addCardImagePlayer.setAttribute('src', obtainCardImg(createCardDeck(suits, values)))
-        playersCards.children[i].appendChild(addCardImagePlayer)
+        shuffleDeck(mazo) //here I am getting a random card 
+        addCardImagePlayer.setAttribute('src', obtainCardImg(mazo)) 
+        playersCards.children[i].appendChild(addCardImagePlayer) //Here I append two cards to the players board. 
+        boardPlayer[i] = 1; // Here I indicate it's occupied placing a one in position boardPlayer[i]
+        sum1=sum1+obtainCardValue(mazo)
+        console.log("player"+ sum1)
+        
+        // this renders the board for the dealer 
         const addCardImageDealer = document.createElement('img')
-        shuffleDeck(createCardDeck(suits, values))
-        addCardImageDealer.setAttribute('src', obtainCardImg(createCardDeck(suits, values)))
-        dealersCards.children[i].appendChild(addCardImageDealer)
+        shuffleDeck(mazo)// here I find a shuffled card from the deck
+        addCardImageDealer.setAttribute('src', obtainCardImg(mazo))
+        dealersCards.children[i].appendChild(addCardImageDealer) //Here i append two cards to the dealers board
+        boardDealer[i] = 1; // Here I indicate it's occupied placing a one in position boardPlayer[i]
+        sum2=sum2+obtainCardValue(mazo)
+        console.log("computer"+sum2)
+
     }
+
+
+}
+
+function renderPlayerChoice(){
+
+        const addCardImagePlayer = document.createElement('img')
+        shuffleDeck(mazo)
+        addCardImagePlayer.setAttribute('src', obtainCardImg(mazo))
+        let indice=boardPlayer.indexOf(0) // Here I obtain the index of the array where it finds the firs 0
+        playersCards.children[indice].appendChild(addCardImagePlayer) // Here I create the image of the index with the first 0 that I found in board player. 
+        boardPlayer[indice]=1
 }
 
 
 
+console.log("this is mazo"+ mazo)
